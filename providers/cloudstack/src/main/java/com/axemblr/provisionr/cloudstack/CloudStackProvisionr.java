5af60de30619525550f98f5d7b9b7ccc881e7419
Andrei Savu <asavu@apache.org>
2012-11-20 00:32:07 +0200

    Completed Karaf feature install

--- providers/cloudstack/src/main/java/com/axemblr/provisionr/cloudstack/CloudStackProvisionr.java
+++ providers/cloudstack/src/main/java/com/axemblr/provisionr/cloudstack/CloudStackProvisionr.java
@@ -5,6 +5,10 @@ import com.axemblr.provisionr.api.pool.Pool;
 
 public class CloudStackProvisionr implements Provisionr {
 
+    public void init() {
+        System.out.println("**** CloudStack (init)");
+    }
+
     @Override
     public void createPool(String id, Pool pool) {
         System.out.println("**** CloudStack (createPool) id: " + id + " pool: " + pool);
