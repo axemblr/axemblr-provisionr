75e913140b1a98a959cbcfb3bccef7ed9a68298c
Andrei Savu <asavu@apache.org>
2013-01-26 12:24:41 +0200

    provisionr:destroy should only need the pool key

--- karaf/commands/src/main/java/com/axemblr/provisionr/commands/DestroyPoolCommand.java
+++ karaf/commands/src/main/java/com/axemblr/provisionr/commands/DestroyPoolCommand.java
@@ -18,12 +18,15 @@ package com.axemblr.provisionr.commands;
 
 import com.axemblr.provisionr.api.Provisionr;
 import com.axemblr.provisionr.commands.predicates.ProvisionrPredicates;
+import com.axemblr.provisionr.core.CoreProcessVariables;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
 import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.collect.Iterables;
 import java.util.List;
 import java.util.NoSuchElementException;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.apache.felix.gogo.commands.Command;
 import org.apache.felix.gogo.commands.Option;
 import org.apache.karaf.shell.console.OsgiCommandSupport;
@@ -31,34 +34,38 @@ import org.apache.karaf.shell.console.OsgiCommandSupport;
 @Command(scope = "provisionr", name = "destroy", description = "Destroy pool")
 public class DestroyPoolCommand extends OsgiCommandSupport {
 
-    @Option(name = "--id", description = "Provisioning service ID", required = true)
-    private String id;
-
-    @Option(name = "-k", aliases = "--key", description = "Business pool key", required = true)
+    @Option(name = "-k", aliases = "--key", description = "Pool key", required = true)
     private String businessKey;
 
     private final List<Provisionr> services;
+    private final ProcessEngine processEngine;
 
-    public DestroyPoolCommand(List<Provisionr> services) {
+    public DestroyPoolCommand(List<Provisionr> services, ProcessEngine processEngine) {
         this.services = checkNotNull(services, "services is null");
+        this.processEngine = checkNotNull(processEngine, "processEngine is null");
     }
 
     @Override
     protected Object doExecute() throws Exception {
-        Optional<Provisionr> service = Iterables.tryFind(services, ProvisionrPredicates.withId(id));
+        checkNotNull(businessKey, "pool business key is mandatory");
+
+        ProcessInstance instance = processEngine.getRuntimeService().createProcessInstanceQuery()
+            .processInstanceBusinessKey(businessKey).singleResult();
+        checkNotNull(instance, "no pool found with key " + businessKey);
+
+        String providerId = (String) processEngine.getRuntimeService()
+            .getVariable(instance.getId(), CoreProcessVariables.PROVIDER);
+        checkNotNull(providerId, "the process instance has no provider ID");
+
+        Optional<Provisionr> service = Iterables.tryFind(services, ProvisionrPredicates.withId(providerId));
 
         if (service.isPresent()) {
             service.get().destroyPool(businessKey);
         } else {
-            throw new NoSuchElementException("No provisioning service found with id: " + id);
+            throw new NoSuchElementException("No provisioning service found with id: " + providerId);
         }
 
         return null;
-    }
-
-    @VisibleForTesting
-    void setId(String id) {
-        this.id = id;
     }
 
     @VisibleForTesting
--- karaf/commands/src/test/java/com/axemblr/provisionr/commands/DestroyPoolCommandTest.java
+++ karaf/commands/src/test/java/com/axemblr/provisionr/commands/DestroyPoolCommandTest.java
@@ -17,10 +17,16 @@
 package com.axemblr.provisionr.commands;
 
 import com.axemblr.provisionr.api.Provisionr;
+import com.axemblr.provisionr.core.CoreProcessVariables;
 import com.google.common.collect.ImmutableList;
 import java.util.NoSuchElementException;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.runtime.ProcessInstanceQuery;
 import org.apache.felix.service.command.CommandSession;
 import org.junit.Test;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -30,14 +36,16 @@ public class DestroyPoolCommandTest {
     public static final String TEST_PROVISIONR_ID = "amazon";
 
     public static final String TEST_BUSINESS_KEY = "dummy";
+    public static final String PROCESS_INSTANCE_ID = "123";
 
     @Test
     public void testDestroyPool() throws Exception {
         Provisionr service = mock(Provisionr.class);
         when(service.getId()).thenReturn(TEST_PROVISIONR_ID);
 
-        DestroyPoolCommand command = new DestroyPoolCommand(ImmutableList.of(service));
-        command.setId(TEST_PROVISIONR_ID);
+        ProcessEngine processEngine = mockProcessEngine(PROCESS_INSTANCE_ID, TEST_BUSINESS_KEY, TEST_PROVISIONR_ID);
+
+        DestroyPoolCommand command = new DestroyPoolCommand(ImmutableList.of(service), processEngine);
         command.setBusinessKey(TEST_BUSINESS_KEY);
 
         CommandSession session = mock(CommandSession.class);
@@ -48,11 +56,32 @@ public class DestroyPoolCommandTest {
 
     @Test(expected = NoSuchElementException.class)
     public void testFailsWithAnEmptyServiceList() throws Exception {
-        DestroyPoolCommand command = new DestroyPoolCommand(ImmutableList.<Provisionr>of());
-        command.setId(TEST_PROVISIONR_ID);
+        ProcessEngine processEngine = mockProcessEngine(PROCESS_INSTANCE_ID, TEST_BUSINESS_KEY, TEST_PROVISIONR_ID);
+
+        DestroyPoolCommand command = new DestroyPoolCommand(ImmutableList.<Provisionr>of(), processEngine);
 
         CommandSession session = mock(CommandSession.class);
+        command.setBusinessKey(TEST_BUSINESS_KEY);
         command.execute(session);
+    }
+
+    public ProcessEngine mockProcessEngine(String processInstanceId, String businessKey, String providerId) {
+        ProcessInstance instance = mock(ProcessInstance.class);
+        when(instance.getId()).thenReturn(processInstanceId);
+
+        ProcessInstanceQuery query = mock(ProcessInstanceQuery.class);
+        when(query.singleResult()).thenReturn(instance);
+        when(query.processInstanceBusinessKey(eq(businessKey))).thenReturn(query);
+
+        RuntimeService runtimeService = mock(RuntimeService.class);
+        when(runtimeService.createProcessInstanceQuery()).thenReturn(query);
+        when(runtimeService.getVariable(eq(processInstanceId), eq(CoreProcessVariables.PROVIDER)))
+            .thenReturn(providerId);
+
+        ProcessEngine processEngine = mock(ProcessEngine.class);
+        when(processEngine.getRuntimeService()).thenReturn(runtimeService);
+
+        return processEngine;
     }
 
 }
